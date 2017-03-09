771b341236298b0be3eeed3756bdc3db2e4a28e4
perlsmith <https://github.com/perlsmith/GitExample.git>
2017-03-09 08:49:02 -0500

    comments about how

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,7 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// hack got no mult, so obviously it's repeated addition - for loop
+// most efficient alkorezm will use the smaller number for the loop
+

9489ce3c245cb1ee2b236e237949de4b1540b223
perlsmith <https://github.com/perlsmith/GitExample.git>
2017-03-10 21:40:48 -0500

    Minimum works okay

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -11,3 +11,41 @@
 // hack got no mult, so obviously it's repeated addition - for loop
 // most efficient alkorezm will use the smaller number for the loop
 
+// find smaller number here
+	@R2
+	M=0			// null this out
+	@R0
+	D=M
+	@counter
+	M=D			// until we find that R1 is smaller
+	@R1		// now store R1 in R2
+	D=M
+	@R2
+	M=D
+	@R0		// now we compareth
+	D=M-D
+	@R1SMALLER
+	D, JGT		// if R1 is smaller than R0, this will be > 0 ..
+	@LOOP
+	0, JMP			// start multiplying
+
+(R1SMALLER)
+	@R1
+	D=M
+	@counter
+	M=D
+	@R0
+	D=M
+	@R2
+	M=D
+
+// for loop here
+(LOOP)
+	@counter
+	D=A-1
+	@END
+	D, JEQ		// if we're at 0, we're done mate
+
+
+(END)
+	@END

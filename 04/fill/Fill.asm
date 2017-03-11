9361918db6d266abf7642696642d1a853443c407
perlsmith <https://github.com/perlsmith/GitExample.git>
2017-03-11 09:49:42 -0500

    preliminary fill proj - only keyboard polling working

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,59 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(POLL_WHILE_CLEAR)
+	@KBD
+	D=M
+	@POLL_WHILE_CLEAR
+	D, JEQ
+				// not zero implies a key pressed
+	@pix_val
+	M=-1
+	@PAINT
+	0, JMP
+
+(POLL_WHILE_BLACK)
+	@KBD
+	D=M
+	@POLL_WHILE_BLACK
+	D, JNE
+				// else, zero implies no key pressed anymore, so clear the screen
+	@pix_val
+	M=0
+	@PAINT
+	0, JMP
+	
+(PAINT)
+	@8192
+	D=A
+	@counter
+	M=D
+(LOOP_DRAW)
+	@counter
+	MD=M-1
+	@DRAW_DONE
+	D, JEQ		// if we're 0, we're done
+	@SCREEN
+	D=A
+	@counter
+	D=M+D		// finally the word we're going to be writing to..
+	@scr_pointer
+	M=D
+	@pix_val
+	D=M
+	@scr_pointer
+	A=M
+	M=D
+	@LOOP_DRAW
+(DRAW_DONE)
+	@pix_val
+	D=M
+	@POLL_WHILE_CLEAR
+	D, JEQ		// if you were writing 0's then you just cleared the screeen..
+	@POLL_WHILE_BLACK
+	D, JMP
+
+(END)
+	@END
+	0, JMP

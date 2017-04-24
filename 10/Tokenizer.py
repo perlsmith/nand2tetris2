541d065a4bb250d716b23cb012b3a600468dc01f
Ananth Chellappa <ananth.ch@gmail.com>
2017-04-23 22:50:30 -0400

    Square okay, but SquareGame bad - block comments not good..

--- 10/Tokenizer.py
+++ 10/Tokenizer.py
@@ -17,10 +17,30 @@ class Parser():
 		self.instream = open( filename, "r")	# be nice to do some exception handling :)
 		# need to support directories - pending..
 		self.nextline = ''
+		self.mode = 'normal'		# to handle block comments - the tough guys :)
+
+# handling comments is the hard part
+#  ..... */   ..... /* -- if you were in comment mode, then you have to stay in comment mode :)
 		
 	def hasMoreAtoms( self ):
 		if ( '' == self.nextline ) :
 			self.nextline = self.instream.readline();
+			if( 'normal' == self.mode ) :
+				self.nextline = re.sub( r"/\*.+?\*/" , '' , self.nextline ) # in a non-greedy way, swallow up all comments
+				# only allowed to do this in normal mode - if you were already in comment mode, you'd have to eat up everything
+				# till */ :)
+				if( re.search( r"/\*" , self.nextline ) ) :	# something escaped the earlier lunch..
+					self.mode = "comment"
+					self.nextline = re.sub ( r"/\*.+" , '' , self.nextline )
+			elif( 'comment' == self.mode ) :
+				if ( re.search( r"\*/" , self.nextline ) ) :
+					self.mode = 'normal'
+				self.nextline = re.sub( r"^.+?\*/" , '' , self.nextline )	# now, swallow up all till the terminator
+				self.nextline = re.sub( r"/\*.+?\*/" , '' , self.nextline ) # in a non-greedy way, swallow up all comments
+				if( re.search( r"/\*" , self.nextline ) ) :	# something escaped the earlier lunch..
+					self.mode = "comment"
+					self.nextline = re.sub ( r"/\*.+" , '' , self.nextline )				
+			self.nextline = re.sub( r"//.+$" , "" , self.nextline )
 		else : 		# there are tokens to process
 			return True
 			
@@ -48,15 +68,15 @@ class TknWriter() :
 	def writeToken( self, type, value ) :	# WORD, stringConstant, SYM, integerConstant and WORD will generate identifier or keyword..
 		if( "WORD" == type ) :
 			if( value in self.keywds ) :
-				self.outstream.write( "\t" + '<keyword> ' + value + " </keyword>\n" )
+				self.outstream.write( "" + '<keyword> ' + value + " </keyword>\n" )
 			else :
-				self.outstream.write( "\t" + '<identifier> ' + value + " </identifier>\n" )
+				self.outstream.write( "" + '<identifier> ' + value + " </identifier>\n" )
 		elif ( "SYM" == type ) :
 			if( value in self.specials.keys() ) :
 				value = self.specials[value]
-			self.outstream.write( "\t" + '<symbol> ' + value + " </symbol>\n" )
+			self.outstream.write( "" + '<symbol> ' + value + " </symbol>\n" )
 		else :
-			self.outstream.write( "\t" + '<' + type + '> ' + value + ' </' + type + ">\n" )
+			self.outstream.write( "" + '<' + type + '> ' + value + ' </' + type + ">\n" )
 		
 
 	def Close( self ) :
@@ -107,7 +127,7 @@ for file in filelist :
 			elif ( '"' ==  atom ) :
 				state = "STRCONST"
 		elif ( 'WORD' == state ) :
-			if ( re.match( r"_[0-9a-zA-Z]" , atom ) ) :
+			if ( re.match( r"[_0-9a-zA-Z]" , atom ) ) :
 				buffer = buffer + atom
 			elif ( re.match( r"[{}().,;+-\[\]/&|<>=~]|\*" , atom ) ) :
 				j_TknWriter.writeToken( "WORD" , buffer )

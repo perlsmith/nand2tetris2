666e0e9e3cb75b98bd102efb97d27ea622559a0e
Ananth Chellappa <ananth.ch@gmail.com>
2017-05-13 17:44:51 -0700

    test varDec *; not done; fix bad wrap ; also type --> _type

--- 10/SyntaxAnalyzer.py
+++ 10/SyntaxAnalyzer.py
@@ -31,13 +31,13 @@ class Analyzer():
 		# note : 1 => 1, 2 => 0 or 1 (?) , 3 => 0 or more (*)
 		self.rules['class'] = ['class' , 1, 'className' , 1, '{' , 1 ,  'classVarDec' , 3, 'subroutineDec' , 3 , '}' , 1 ]
 		self.elements['class'] = ['keyword', 'identifier' , 'symbol', 'rule' , 'rule' , 'symbol' ]
-		self.rules['classVarDec'] = ['static|field' , 1 , 'type' , 1 , 'varName' , 1 , '_addlVarDec', 3  , ';' , 1 ]
+		self.rules['classVarDec'] = ['static|field' , 1 , '_type' , 1 , 'varName' , 1 , '_addlVarDec', 3  , ';' , 1 ]
 		self.elements['classVarDec'] = ['keyword' , 'rule' , 'identifier' , 'rule', 'symbol']
 		self.rules['_addlVarDec'] = [',' , 1, 'varName', 1 ]		# _name implies this rule will not generate a token
 		self.elements['_addlVarDec'] = ['symbol', 'identifier']
-		self.rules['type'] = ['int|char|boolean||className' , 1]
-		self.elements['type'] = ['keyword||identifier']
-		self.rules['subroutineDec'] = ['constructor|function|method' , 1 , 'void||type' , 1 , 'subroutineName' , 1 , '(', 1, 'parameterList' , 1 , ')' , 1, 'subroutineBody' , 1]
+		self.rules['_type'] = ['int|char|boolean||className' , 1]
+		self.elements['_type'] = ['keyword||identifier']
+		self.rules['subroutineDec'] = ['constructor|function|method' , 1 , 'void||_type' , 1 , 'subroutineName' , 1 , '(', 1, 'parameterList' , 1 , ')' , 1, 'subroutineBody' , 1]
 		self.elements['subroutineDec'] = ['keyword' , 'keyword||rule' , 'identifier' , 'symbol' , 'rule', 'symbol', 'rule' ]
 		# what this means is that you first look for keyword : void - if you see void, then your put down <keyword> void </keyword> else
 		# you look at type - which is again looking for keyword : int|char|boolean .... you get the idea..
@@ -46,13 +46,13 @@ class Analyzer():
 		self.elements['parameterList'] = ['rule']
 		self.rules['_params'] = [ '_param' , 1 , '_addlParam' , 3 ]
 		self.elements['_params'] = ['rule' , 'rule' ]
-		self.rules['_param'] = ['type' , 1, 'varName' , 1 ]
+		self.rules['_param'] = ['_type' , 1, 'varName' , 1 ]
 		self.elements['_param'] = ['rule', 'identifier']
 		self.rules['_addlParam' ] = [ ',' , 1 , 'varName' , 1]
 		self.elements['_addlParam' ] = [ 'symbol' , 'identifier' ]
 		self.rules['subroutineBody'] = ['{' , 1 , 'varDec' , 3 , 'statements' , 1 , '}' , 1 ]
 		self.elements['subroutineBody'] = ['symbol' , 'rule', 'rule', 'symbol' ]
-		self.rules['varDec'] = ['var' , 1, 'type' , 1, 'varName' , 1 , '_addlVarDec' , 3 , ';' , 1 ]
+		self.rules['varDec'] = ['var' , 1, '_type' , 1, 'varName' , 1 , '_addlVarDec' , 3 , ';' , 1 ]
 		self.elements['varDec'] = ['keyword' , 'rule' , 'identifier' , 'rule' , 'symbol' ]
 		self.rules['statements'] = ['_statement' , 3 ]	# this was a curve ball - didn't realize they don't want <statement> ha!
 		self.elements['statements'] = ['rule']
@@ -120,12 +120,6 @@ class Analyzer():
 	# this is the main operator that uses other methods - maybe an OO noob style deprecated, but..	
 	# elements[xyz][] -- if you see 'rule', that results in another call to analyze()
 	#					-- if you see || then you split on || and process the resulting list in OR fashion - first one that hits terminates
-	# open question at this point - how do you know if you should terminate with an error or if you
-	# are in a ? or * so you just move on to the next thing? You need a token buffer where you store
-	# stuff so you can backtrack - when you're in a ? or * - so the next rule can use what you've read in so far.. :) -- the LL2 helps out
-	# you only want to issue an error when you MUST see something - if you're within a ? or *, you can't ERROR out :)
-	# we'll have to update so that we have a single register token stack.. we first look there before
-	# reading a new token from the file..
 	# also, hunger can only get elevated when traversing a rule laterally -- going by LL1..
 	def analyze( self, ruleName, hunger, severe=False ) :		# hunger is the same as 1,2,3 for 1, ?, *
 		# returns a buffer containing tokens satisfying rule, or ''. If return is '', then 
@@ -139,6 +133,7 @@ class Analyzer():
 		rule = self.rules[ruleName]		# remember, .rules is a dict, and each value is a list of elements
 		whatIs = self.elements[ruleName]	# now, whatIs tells you what each element of rule is - what type..
 		numR = len( rule ) >> 1		# dividing by 2 gets you # of sub-rules
+		howMany = 0;
 		
 		while( appetite ) :
 			depth = 0		# local depth -- as you move from left to right, you have to increment
@@ -163,14 +158,18 @@ class Analyzer():
 							subMatch = self.analyze( rTypes[j] , count, depth>0 )	# the recursive call. severity set on the fly
 							if( not ( '' == subMatch ) ):
 								satisfied = True
-								buffer = buffer + subMatch
 								if( not re.match( '_' , rTypes[j] ) ) :
-									buffer = '<' + rTypes[j] + ">\n" + buffer + '</' + rTypes[j] + ">\n"
+									subMatch = '<' + rTypes[j] + ">\n" + subMatch + '</' + rTypes[j] + ">\n"
+								buffer = buffer + subMatch
+							if( '' == subMatch and 1 < hunger ) :
+								satisfied = True	# question : do we ever have xyz||rule with ?/*?
 						else : 	# not a rule, so match immediately.. good news is that hunger only applies to rules :)
 							if ( self.hasMoreTokens() ) :
 								if ( self.tokenName == type and re.match( rTypes[j] , self.token ) ) :
 									satisfied = True
 									buffer = buffer + self.nextline		# doesn't sound pretty, but..
+									self.tokenName = ''	# no going back now.. with anything but a rule, you *have* to match..
+													# this is where the token is consumed
 					j = j + 1
 
 	# example of back-tracking - varDec* - you see one variable declaration, but you're hungry for more
@@ -184,6 +183,10 @@ class Analyzer():
 				
 				depth = depth + 1
 				
+			if( satisfied ) :
+				howMany = howMany + 1
+				if( 3 > hunger ) :
+					appetite = False
 			
 		return buffer
 			# in the case of 2 or 3, you only add whatIs if you actually find the patterns..
@@ -247,12 +250,11 @@ else :
 		sys.exit()
 
 
-
+pdb.set_trace()
 for file in filelist :
 	j_analyzer = Analyzer( file )	# this does an init and also open the target for writing..
 #	j_analyzer.Write( j_analyzer.analyze('class') )	# will also write
-	print( j_analyzer.analyze('_unOpTerm' , 1) )
-	print( j_analyzer.analyze('_unOpTerm' , 1) )
+	print( j_analyzer.analyze('varDec' , 3) )
 
 
 		

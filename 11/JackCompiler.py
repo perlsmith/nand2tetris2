a6ef171546f0dc194ef86f98548591e449cdf6ca
Ananth Chellappa <ananth.ch@gmail.com>
2017-08-20 23:00:45 -0400

    fixed if and while to use local and update global if_lbl_id prior to processing

--- 11/JackCompiler.py
+++ 11/JackCompiler.py
@@ -140,10 +140,11 @@ class Analyzer():
 		
 		self.rules['ifStatement'] = ['if' , 1 , '\(' , 1 , 'expression' , 1 , '\)' , 1 , '{' , 1 , 'statements' , 1 , '}' , 1 , '_elseBlock' , 2 ]
 		self.elements['ifStatement'] = ['keyword' , 'symbol', 'rule', 'symbol', 'symbol', 'rule' , 'symbol' , 'rule' ]
-		self.toDo['ifStatement'] = [0, 'n/a', -2, 'VMbuf[7] = "label LBL_IF_" + str(self.if_lbl_id+2)', 
-										0, 'dump', -2, "VMbuf[1] = 'not'\nVMbuf[2] = 'if-goto LBL_IF_' + str(self.if_lbl_id+1)",
+		self.toDo['ifStatement'] = [-2, 'if_lbl_id=self.if_lbl_id\nself.if_lbl_id += 2', 		# keyword 'if'
+									-2, 'VMbuf[7] = "label LBL_IF_" + str(if_lbl_id+1)', 
+										0, 'dump', -2, "VMbuf[1] = 'not'\nVMbuf[2] = 'if-goto LBL_IF_' + str(if_lbl_id)",
 										0, 'n/a', 6, 'dump', 
-										-2, "VMbuf[4] = 'goto LBL_IF_'+str(self.if_lbl_id+2)\nVMbuf[5] = 'label LBL_IF_' + str(self.if_lbl_id+1) \nself.if_lbl_id += 2",
+										-2, "VMbuf[4] = 'goto LBL_IF_'+str(if_lbl_id+1)\nVMbuf[5] = 'label LBL_IF_' + str(if_lbl_id)",
 										3, 'dump']
 		# the implementation uses the counter if_lbl_id and increments it twice as it is used
 		# it's spaghetti code - the analyze function in this class maintains a VMbuf array - and this array contains python code to
@@ -155,10 +156,10 @@ class Analyzer():
 		
 		self.rules['whileStatement'] = ['while', 1 , '\(' , 1, 'expression' , 1 , '\)' , 1 , '{' , 1 , 'statements' , 1 , '}' , 1 ]
 		self.elements['whileStatement'] = ['keyword' , 'symbol' , 'rule', 'symbol' , 'symbol' , 'rule' , 'symbol' ]
-		self.toDo['whileStatement'] = [ -2, "VMbuf[0] = 'label LBL_IF_'+str(self.if_lbl_id+1)",
-										-2, "VMbuf[4] = 'goto LBL_IF_'+str(self.if_lbl_id+1)" , 1, 'dump',
-										-2, "VMbuf[2] = 'if-goto LBL_IF_' + str(self.if_lbl_id+2)", 0, 'n/a', 3, 'dump',
-										-2, "VMbuf[5] = 'label LBL_IF_' + str(self.if_lbl_id+2) \nself.if_lbl_id += 2"]
+		self.toDo['whileStatement'] = [ -2, "if_lbl_id=self.if_lbl_id\nself.if_lbl_id += 2\nVMbuf[0] = 'label LBL_IF_'+str(if_lbl_id)",
+										-2, "VMbuf[4] = 'goto LBL_IF_'+str(if_lbl_id)" , 1, 'dump',
+										-2, "VMbuf[2] = 'if-goto LBL_IF_' + str(if_lbl_id+1)", 0, 'n/a', 3, 'dump',
+										-2, "VMbuf[5] = 'label LBL_IF_' + str(if_lbl_id+1)"]
 		
 		self.rules['doStatement'] = ['do' , 1 , '_subroutineCall' , 1 , ';' , 1 ]
 		self.elements['doStatement'] = ['keyword' , 'rule' , 'symbol' ]
@@ -284,6 +285,7 @@ class Analyzer():
 		hits = [False] * numR
 		capture = ''	# intended for use by exec
 		self.a_index = ''	# supporting spaghetti --> another routine buried somewhere depends on this being reset :)
+		if_lbl_id = 0		# a local that will hold the global during processing of the ifStatement (and whileStatement)
 
 		while( appetite ) :
 			VMbuf = [''] * numR
